3c94729042b213fb6819f62cedc1316dd296dcb0
ibrahim köprücü <43587170+ibrahimkoprucu@users.noreply.github.com>
2019-04-02 02:37:19 +0300

    initial commit

---
+++ SearchForValue/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SearchForValue
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+
+            string x;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = i;
+            }
+
+            do
+            {
+                Console.WriteLine("Input integer value to control :");
+                x = Console.ReadLine();
+                int y;
+                if (int.TryParse(x, out y))
+                {
+                    if (IncludeValue(y, nums))
+                    {
+                        Console.WriteLine("Include");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Include");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("integer value needed!!");
+                }
+
+                Console.WriteLine("Input Q to quit");
+            } while (Console.ReadLine() != "Q");
+        }
+
+        private static bool IncludeValue(int i, int[] ar)
+        {
+            bool found = false;
+
+            foreach (var x in ar)
+            {
+                if (x == i)
+                {
+                    found = true;
+                }
+            }
+            return found;
+        }
+    }
+}
---
+++ SomeProblems/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SomeProblems
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] ar = { 1, 4, 9, 4, 5, 6, 9, 3 };
+            FindSecondLargest(ar);
+        }
+
+        /// <summary>
+        /// Finds second largest element in a given array
+        /// </summary>
+        /// <param name="ar"></param>
+        /// <param name="size"></param>
+        private static void FindSecondLargest(int[] ar)
+        {
+            if (ar.Length < 2)
+            {
+                Console.WriteLine("Invalid Input");
+                return;
+            }
+
+            int first, second;
+            int indexOfFirst = 0;
+            int indexOfSecond = 0;
+
+            first = second = ar[0];
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] > first)
+                {
+                    second = first;
+                    indexOfSecond = indexOfFirst;
+                    first = ar[i];
+                    indexOfFirst = i;
+                }
+                else if (ar[i] > second && ar[i] != first)
+                {
+                    second = ar[i];
+                    indexOfSecond = i;
+                }
+            }
+
+            Console.WriteLine("Largest element:{0},second Largest element:{1}", first, second);
+            Console.WriteLine(@"Index of Largest element:{0},Index of
+                              second Largest element:{1}", indexOfFirst, indexOfSecond);
+        }
+    }
+}
